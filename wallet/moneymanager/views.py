6eb17b0d9c243111c5a0ef34748dd7536542db08
arham musheer <arhammusheer@gmail.com>
2019-11-19 00:22:18 +0530

    stuff

--- wallet/moneymanager/views.py
+++ wallet/moneymanager/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from django.contrib.auth import authenticate, login
+
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
